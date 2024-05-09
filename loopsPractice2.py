25fdf78bad111ea4a648c306b8665c59105a96b3
Valles <ldvalles@cps.k12.il.us>
2024-05-09 08:40:06 -0500

    first commit

--- loopsPractice2.py
+++ loopsPractice2.py
@@ -2,4 +2,8 @@ list1 = range(1,1001)
 for number in list1:
     if number % 2 ==0:
         print(f'number is even{number}')
-        
+else:
+    print("This number is not even")
+
+
+print(f'the sum of the list is (sum(list1))')
